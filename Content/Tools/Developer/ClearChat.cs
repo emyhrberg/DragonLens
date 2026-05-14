08cac8ec94534b5e8f6311319f2c33be9c765436
emyhrberg <121192176+emyhrberg@users.noreply.github.com>
2026-05-14 06:48:47 +0200

    Added clear chat and leave tool

---
+++ Content/Tools/Developer/ClearChat.cs
@@ -0,0 +1,54 @@
+﻿using DragonLens.Core.Systems.ToolSystem;
+using MonoMod.Logs;
+using System;
+using System.Reflection;
+using Terraria.Chat;
+using Terraria.GameContent.UI.Chat;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.UI;
+
+namespace DragonLens.Content.Tools.Developer
+{
+	public class ClearChat : Tool
+	{
+		public override string IconKey => "NoIconForThisDebugTool";
+
+		public override string DisplayName => "Clear chat";
+
+		public override string Description => "Clears the Terraria chat";
+
+		public override void OnActivate()
+		{
+			// Clear Main.chatMonitor messages
+			MethodInfo clearMethod = Main.chatMonitor.GetType().GetMethod("Clear", BindingFlags.Instance | BindingFlags.Public);
+
+			// Try to clear in 3 different ways but only 30 empty messages works lol.
+			if (clearMethod != null)
+			{
+				// attempt 1
+				(Main.chatMonitor as RemadeChatMonitor)?.Clear();
+
+				// attempt 2
+				clearMethod?.Invoke(Main.chatMonitor, null);
+
+				// attempt 3
+				for (int i = 0; i < 30; i++)
+				{
+					ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(string.Empty), Color.White);
+				}
+				//DebugLog.Chat("Chat cleared!!");
+			}
+		}
+		public override void DrawIcon(SpriteBatch sb, Rectangle position)
+		{
+			//base.DrawIcon(sb, position);
+
+			// Draw announcement box icon
+			var item = ContentSamples.ItemsByType[ItemID.AnnouncementBox];
+			var pos = position.Center.ToVector2();
+
+			ItemSlot.DrawItemIcon(item, ItemSlot.Context.InventoryItem, sb, pos, 0.8f, Math.Min(position.Width, position.Height), Color.White);
+		}
+	}
+}
---
+++ Content/Tools/Developer/Leave.cs
@@ -0,0 +1,32 @@
+﻿using DragonLens.Core.Systems.ToolSystem;
+using System;
+using Terraria.ID;
+using Terraria.UI;
+
+namespace DragonLens.Content.Tools.Developer
+{
+	public class Leave : Tool
+	{
+		public override string IconKey => "NoIconForThisDebugTool";
+
+		public override string DisplayName => "Exit world";
+
+		public override string Description => "Instantly leave to main menu without saving";
+
+		public override void OnActivate()
+		{
+			WorldGen.JustQuit();
+		}
+
+		public override void DrawIcon(SpriteBatch sb, Rectangle position)
+		{
+			//base.DrawIcon(sb, position);
+
+			// Draw wooden door icon
+			var item = ContentSamples.ItemsByType[ItemID.WoodenDoor];
+			var pos = position.Center.ToVector2();
+
+			ItemSlot.DrawItemIcon(item, ItemSlot.Context.InventoryItem, sb, pos, 1f, Math.Min(position.Width, position.Height), Color.White);
+		}
+	}
+}
